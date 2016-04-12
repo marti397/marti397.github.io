bd79a731cffc71c62b76175eb2f91bc0ee6ebf6e
marti397 <chris.m.servin@gmail.com>
2016-04-12 13:39:31 -0500

    changes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
-var color = 1;
-$("#colorChange").click(function(){
-    if (color === 1){
-        $("#colorChange").css("color","blue");
-        color = 2;
+$("li").append("<span class='circleNo'>&#x2717;</span>")
+$("li").addClass("boxNo");
+$("span").click(function(){
+    if($(this).html()===document.getElementById("siTrue").innerHTML){
+        $(this).html('&#x2717;');
+        $(this).removeClass("circleYes");
+        $(this).parent().removeClass("boxYes");
+        $(this).addClass("circleNo");
+        $(this).parent().addClass("boxNo");
     }else{
-        $("#colorChange").css("color","green");
-        color = 1;
+        $(this).html('&#x2713;');
+        $(this).removeClass("circleNo");
+        $(this).parent().removeClass("boxNo");
+        $(this).addClass("circleYes");
+        $(this).parent().addClass("boxYes");
     }
- });
+})
