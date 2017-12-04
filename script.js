da6a37757c1a8aaddb5fc99897a617fdedc3f455
chrm11 <chrm11@Christophers-MacBook-Pro.local>
2017-12-03 23:26:23 -0600

    Changes to Website

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+$(document).ready(function($) {
+    $('#accordion').find('.accordion-toggle').click(function(){
+        //Expand or collapse this panel
+        $(this).next().slideToggle('fast');
+        //Hide the other panels
+        $(".accordion-content").not($(this).next()).slideUp('fast');
+    });
+});
+
